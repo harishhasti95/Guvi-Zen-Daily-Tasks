eab9023217379073e01bfc471d93a4f07093572b
harish hasti <harishhasti95@gmail.com>
2020-09-10 15:35:49 +0530

    Update string_caps.js

--- 09-09-2020/Arrow Functions/string_caps.js
+++ 09-09-2020/Arrow Functions/string_caps.js
@@ -1,3 +1,4 @@
+/* Convert all the strings to title caps in a string array */
 var str = 'haris is sleeping after he ate'
 str = str.split(' ');
 str = str.map(x => x.charAt(0).toUpperCase()+x.slice(1))
