afa373c37863789f28886bc612b8852fd3529dd9
harish hasti <harishhasti95@gmail.com>
2020-09-10 15:38:49 +0530

    Update pallindrome.js

--- 09-09-2020/Arrow Functions/pallindrome.js
+++ 09-09-2020/Arrow Functions/pallindrome.js
@@ -1,2 +1,7 @@
-let a = (['sararas','lkj','lkkl'];
-console.log(a);
+var arr = ["hari", "aua", "refer", "tabat"];
+var palindromes = arr.filter((str) => {
+	var re = /[^A-Za-z0-9]/g;
+	var modString = str.toLowerCase().replace(re, '');
+	return (modString == modString.split('').reverse().join(''))
+});
+console.log(palindromes);
