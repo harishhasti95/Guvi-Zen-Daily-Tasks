7caf836c47e37bc3f8efeea5e313815abc527e34
harish hasti <harishhasti95@gmail.com>
2020-10-10 11:36:07 +0530

    Add files via upload

--- 7-oct/script.js
+++ 7-oct/script.js
@@ -1,59 +1,13 @@
 var available_pets = {
-    Dog: [{ name: 'Gimmy', color: 'white' }, { name: 'Rock', color: 'black' }, { name: 'Snoopy', color: 'dalmation' }, { name: 'snoophy', color: 'gold' }],
+    Dog: [{ name: 'Gimmy', color: 'white' }, { name: 'Rock', color: 'black' }, { name: 'Snoopy', color: 'dalmation' }],
     Cat: [{ name: 'Oliver', color: 'white' }, { name: 'Leo', color: 'black' }, { name: 'kutty', color: 'brownish' }],
     Bird: [{ name: 'ramu', color: 'green' }, { name: 'seetha', color: 'black' }, { name: 'geetha', color: 'yellow' }]
 };
 var petShop = /** @class */ (function () {
     function petShop(pets) {
         this.available_pets = pets;
-        var tab = document.getElementById('available');
-        var a = Object.keys(this.available_pets);
-        for (var i = 0; i < a.length; i++) {
-            var b = this.available_pets[a[i]];
-            console.log(b);
-            for (var j = 0; j < b.length; j++) {
-                var tr = document.createElement('tr');
-                var th = document.createElement('td');
-                th.innerHTML = a[i];
-                var th2 = document.createElement('td');
-                th2.innerHTML = b[j].name;
-                var th3 = document.createElement('td');
-                th3.innerHTML = b[j].color;
-                tr.appendChild(th);
-                tr.appendChild(th2);
-                tr.appendChild(th3);
-                var t = document.getElementById('available');
-                t.appendChild(tr);
-            }
-        }
-        var but = document.getElementById('button');
-        but.onclick = this.checkAvailability();
     }
-    petShop.prototype.checkAvailability = function () {
-        var blah = document.getElementById('animal').value;
-        var keys_a = Object.keys(this.available_pets);
-        if (function () {
-            for (var m = 0; m < keys_a.length; m++) {
-                if (keys_a[m] == blah) {
-                    return true;
-                }
-            }
-            return false;
-        }) {
-            if (this.available_pets[blah] != undefined) {
-                var k = document.getElementById('appending_span');
-                var k1 = document.createElement('span');
-                k1.innerHTML = 'yes, available';
-                k.appendChild(k1);
-                this.available_pets[blah].shift();
-            }
-            else {
-                var k = document.getElementById('appending_span');
-                var k1 = document.createElement('span');
-                k1.innerHTML = 'Not available';
-                k.appendChild(k1);
-            }
-        }
+    petShop.prototype.printAvailability = function () {
         var tab = document.getElementById('available');
         tab.innerHTML = '';
         var a = Object.keys(this.available_pets);
@@ -75,9 +29,35 @@ var petShop = /** @class */ (function () {
                 t.appendChild(tr);
             }
         }
-        console.log(this.available_pets);
-        return this.available_pets;
+    };
+    petShop.prototype.checkAvailability = function () {
+        var _this = this;
+        return function () {
+            var blah = document.getElementById('animal').value;
+            var keys_a = Object.keys(_this.available_pets);
+            var flag = false;
+            for (var r = 0; r < keys_a.length; r++) {
+                if (blah == keys_a[r]) {
+                    flag = true;
+                    console.log(flag);
+                }
+            }
+            if (flag) {
+                if (_this.available_pets[blah] != undefined) {
+                    var k = document.getElementById('is_it_there');
+                    k.innerHTML = 'yes, available';
+                    _this.available_pets[blah].shift();
+                }
+                else {
+                    var k = document.getElementById('is_it_there');
+                    k.innerHTML = 'Not available';
+                }
+                _this.printAvailability();
+            }
+        };
     };
     return petShop;
 }());
 var news = new petShop(available_pets);
+var but = document.getElementById('button');
+but.onclick = news.checkAvailability();
--- 7-oct/script.ts
+++ 7-oct/script.ts
@@ -1,72 +1,16 @@
 var available_pets = {
-    Dog: [{name: 'Gimmy',color: 'white'},{name: 'Rock',color: 'black'},{name: 'Snoopy',color: 'dalmation'},{name:'snoophy',color:'gold'}],
+    Dog: [{name: 'Gimmy',color: 'white'},{name: 'Rock',color: 'black'},{name: 'Snoopy',color: 'dalmation'}],
     Cat: [{name: 'Oliver',color: 'white'},{name: 'Leo',color: 'black'},{name: 'kutty',color: 'brownish'}],
     Bird: [{name: 'ramu',color: 'green'},{name: 'seetha',color: 'black'},{name:'geetha',color:'yellow'}]
 }
-
 class petShop{
     available_pets;
     constructor(pets){
         this.available_pets = pets;
-        let tab = document.getElementById('available');
-        let a = Object.keys(this.available_pets);
-        for(let i=0;i<a.length;i++){
-            let b = this.available_pets[a[i]]
-            console.log(b);
-            for(let j=0;j<b.length;j++){
-                let tr = document.createElement('tr');
-
-                let th = document.createElement('td');
-                th.innerHTML = a[i];
-
-                let th2 = document.createElement('td');
-                th2.innerHTML = b[j].name;
-
-                let th3 = document.createElement('td');
-                th3.innerHTML = b[j].color;
-
-                tr.appendChild(th);
-                tr.appendChild(th2);
-                tr.appendChild(th3);
-                
-                let t = document.getElementById('available');
-                t.appendChild(tr);
-            }
-        }
-
-        let but = document.getElementById('button');
-        but.onclick = this.checkAvailability();
-
+        
     }
-    
-    checkAvailability(){
-        let blah = (<HTMLSelectElement>document.getElementById('animal')).value;
-        let keys_a = Object.keys(this.available_pets);
 
-        if (() => {
-            for(let m=0;m<keys_a.length;m++){
-                if(keys_a[m] == blah){return true}
-            }
-            return false
-        }) {
-            if(this.available_pets[blah] != undefined){
-                let k = document.getElementById('appending_span');
-
-                let k1 = document.createElement('span');
-                k1.innerHTML = 'yes, available';
-
-                k.appendChild(k1);
-
-                this.available_pets[blah].shift();
-            }else{
-                let k = document.getElementById('appending_span');
-
-                let k1 = document.createElement('span');
-                k1.innerHTML = 'Not available';
-
-                k.appendChild(k1);
-            }
-        }
+    printAvailability(){
         let tab = document.getElementById('available');
         tab.innerHTML = '';
         let a = Object.keys(this.available_pets);
@@ -75,29 +19,45 @@ class petShop{
             console.log(b);
             for(let j=0;j<b.length;j++){
                 let tr = document.createElement('tr');
-
                 let th = document.createElement('td');
                 th.innerHTML = a[i];
-
                 let th2 = document.createElement('td');
                 th2.innerHTML = b[j].name;
-
                 let th3 = document.createElement('td');
                 th3.innerHTML = b[j].color;
-
                 tr.appendChild(th);
                 tr.appendChild(th2);
                 tr.appendChild(th3);
-                
                 let t = document.getElementById('available');
                 t.appendChild(tr);
             }
         }
-        console.log(this.available_pets);
-        return this.available_pets
     }
-
+    checkAvailability(): ()=> void{
+        return () =>{
+            let blah = (<HTMLSelectElement>document.getElementById('animal')).value;
+            let keys_a = Object.keys(this.available_pets);
+            let flag = false
+            for(let r=0;r<keys_a.length;r++){
+                if(blah==keys_a[r]){
+                    flag = true;
+                    console.log(flag);
+                }
+            }
+            if(flag){
+                if(this.available_pets[blah] != undefined){
+                    let k = document.getElementById('is_it_there');
+                    k.innerHTML = 'yes, available';
+                    this.available_pets[blah].shift();
+                }else{
+                    let k = document.getElementById('is_it_there');
+                    k.innerHTML = 'Not available';
+                }
+                this.printAvailability()
+            }
+        }
+    } 
 }
-
-
-let news = new petShop(available_pets)
+let news = new petShop(available_pets);
+let but = document.getElementById('button');
+but.onclick = news.checkAvailability();
